3fd5a3226447eea15722238e1087c300ff88050b
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-03-07 21:14:08 +0100

    unity3d: 2017.4.10f1 -> 2018.3.0f2

--- pkgs/development/tools/unity3d/default.nix
+++ pkgs/development/tools/unity3d/default.nix
@@ -1,9 +1,10 @@
 { stdenv, lib, fetchurl, makeWrapper, file, getopt
-, gtk2, gdk_pixbuf, glib, libGL, libGLU, nss, nspr, udev, tbb
+, gtk2, gtk3, gdk_pixbuf, glib, libGL, libGLU, nss, nspr, udev, tbb
 , alsaLib, GConf, cups, libcap, fontconfig, freetype, pango
 , cairo, dbus, expat, zlib, libpng12, nodejs, gnutar, gcc, gcc_32bit
 , libX11, libXcursor, libXdamage, libXfixes, libXrender, libXi
 , libXcomposite, libXext, libXrandr, libXtst, libSM, libICE, libxcb, chromium
+, libpqxx
 }:
 
 let
@@ -13,20 +14,21 @@ let
     cairo dbus expat zlib libpng12 udev tbb
     libX11 libXcursor libXdamage libXfixes libXrender libXi
     libXcomposite libXext libXrandr libXtst libSM libICE libxcb
+    libpqxx gtk3 
   ];
   libPath32 = lib.makeLibraryPath [ gcc_32bit.cc ];
   binPath = lib.makeBinPath [ nodejs gnutar ];
 
-  ver = "2017.4.10";
-  build = "f1";
+  ver = "2018.3.0";
+  build = "f2";
 
 in stdenv.mkDerivation rec {
   name = "unity-editor-${version}";
   version = "${ver}x${build}";
 
   src = fetchurl {
-    url = "https://beta.unity3d.com/download/14396d76537e/LinuxEditorInstaller/Unity.tar.xz";
-    sha256 = "e1b4fe41c0ff793f7a9146c49a8eca8c71d30abdfa3e81922bd69699810b3f67";
+  	url = "https://beta.unity3d.com/download/6e9a27477296/LinuxEditorInstaller/Unity.tar.xz";
+    sha1 = "083imikkrgha5w9sihjvv1m74naxm5yv";
   };
 
   nosuidLib = ./unity-nosuid.c;
@@ -54,38 +56,39 @@ in stdenv.mkDerivation rec {
 
     mkdir -p $out/bin
     makeWrapper $unitydir/Unity $out/bin/unity-editor \
-      --prefix LD_PRELOAD : "$unitydir/libunity-nosuid.so" \
-      --prefix PATH : "${binPath}"
+    --prefix LD_PRELOAD : "$unitydir/libunity-nosuid.so" \
+    --prefix PATH : "${binPath}" \
+    --prefix GTP_IM_MODULE : ""
   '';
 
   preFixup = ''
     patchFile() {
-      ftype="$(file -b "$1")"
-      if [[ "$ftype" =~ LSB\ .*dynamically\ linked ]]; then
-        if [[ "$ftype" =~ 32-bit ]]; then
-          rpath="${libPath32}"
-          intp="$(cat $NIX_CC/nix-support/dynamic-linker-m32)"
-        else
-          rpath="${libPath64}"
-          intp="$(cat $NIX_CC/nix-support/dynamic-linker)"
-        fi
+    ftype="$(file -b "$1")"
+    if [[ "$ftype" =~ LSB\ .*dynamically\ linked ]]; then
+    if [[ "$ftype" =~ 32-bit ]]; then
+    rpath="${libPath32}"
+    intp="$(cat $NIX_CC/nix-support/dynamic-linker-m32)"
+    else
+    rpath="${libPath64}"
+    intp="$(cat $NIX_CC/nix-support/dynamic-linker)"
+    fi
 
-        # Save origin-relative parts of rpath.
-        originRpath="$(patchelf --print-rpath "$1" | sed "s/:/\n/g" | grep "^\$ORIGIN" | paste -sd ":" - || echo "")"
-        rpath="$originRpath:$rpath"
+    # Save origin-relative parts of rpath.
+    originRpath="$(patchelf --print-rpath "$1" | sed "s/:/\n/g" | grep "^\$ORIGIN" | paste -sd ":" - || echo "")"
+    rpath="$originRpath:$rpath"
 
-        patchelf --set-rpath "$rpath" "$1"
-        patchelf --set-interpreter "$intp" "$1" 2> /dev/null || true
-      fi
+    patchelf --set-rpath "$rpath" "$1"
+    patchelf --set-interpreter "$intp" "$1" 2> /dev/null || true
+    fi
     }
 
-    upm_linux=$unitydir/Data/Resources/Upm/upm-linux
+    upm_linux=$unitydir/Data/Resources/PackageManager/Server/UnityPackageManager
 
     orig_size=$(stat --printf=%s $upm_linux)
 
     # Exclude PlaybackEngines to build something that can be run on FHS-compliant Linuxes
     find $unitydir -name PlaybackEngines -prune -o -type f -print | while read path; do
-      patchFile "$path"
+    patchFile "$path"
     done
 
     new_size=$(stat --printf=%s $upm_linux)
@@ -107,13 +110,13 @@ in stdenv.mkDerivation rec {
     shift_by=$(expr $new_size - $orig_size)
 
     function fix_offset {
-      # $1 = name of variable to adjust
-      location=$(grep -obUam1 "$1" $upm_linux | cut -d: -f1)
-      location=$(expr $location + $var_skip)
-      value=$(dd if=$upm_linux iflag=count_bytes,skip_bytes skip=$location \
-                 bs=1 count=$var_select status=none)
-      value=$(expr $shift_by + $value)
-      echo -n $value | dd of=$upm_linux bs=1 seek=$location conv=notrunc
+    # $1 = name of variable to adjust
+    location=$(grep -obUam1 "$1" $upm_linux | cut -d: -f1)
+    location=$(expr $location + $var_skip)
+    value=$(dd if=$upm_linux iflag=count_bytes,skip_bytes skip=$location \
+    bs=1 count=$var_select status=none)
+    value=$(expr $shift_by + $value)
+    echo -n $value | dd of=$upm_linux bs=1 seek=$location conv=notrunc
     }
 
     fix_offset PAYLOAD_POSITION
@@ -132,6 +135,6 @@ in stdenv.mkDerivation rec {
     '';
     license = licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ jb55 ];
+    maintainers = with maintainers; [ jb55 tesq0 ];
   };
 }
